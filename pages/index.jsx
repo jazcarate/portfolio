c7a1ed424d90104d1dd78219aa1a7099f92a847a
Joaquin Florius <j@florius.com.ar>
2020-10-12 19:39:46 +0200

    Fix equiv warning

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ const Index = () => {
     <div id="root">
       <Head lang="en">
         <meta charSet="utf-8" />
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#cccccc" />
 
