8eb8f34de0fc0008498ff78460aab7c21ef6c443
Joaquin Azcarate <j@florius.com.ar>
2023-08-26 12:50:24 +0200

    Better align

--- components/Card/Video.jsx
+++ components/Card/Video.jsx
@@ -14,21 +14,20 @@ export const YouTube = styled.div`
     height: 100%;
     width: 100%;
 
-    background-image:    url(${props => props.$image});
-    background-size:     cover;
-    background-repeat:   no-repeat;
-    background-position: center center;         
+    background-image: url(${props => `http://img.youtube.com/vi/${props.$image}/hqdefault.jpg`}), url(${props => `http://img.youtube.com/vi/${props.$image}/default.jpg`});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center 30%;         
 `;
 
 const VideoCard = ({ name, slug }) => {
     const list = portfolio.videos.playlist;
     const safeName = "video-" + encode(name);
-    const thumbnail = `http://img.youtube.com/vi/${slug}/hqdefault.jpg`;
     const link = `http://www.youtube.com/watch?v=${slug}&list=${list}`;
 
     return (<>
         <CardStock href={link} id={safeName} target="_blank" rel="noopener noreferrer">
-            <YouTube $image={thumbnail} aria-label={name + " in YouTube"} role="img" />
+            <YouTube $image={slug} aria-label={name + " in YouTube"} role="img" />
             <Footer>
                 {name}
             </Footer>
